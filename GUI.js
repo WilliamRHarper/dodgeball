5a398167e310b341ac69dc6698b8a4974e8bc2ef
WilliamRHarper <Wrharpe1230@gmail.com>
2020-06-22 17:13:17 -0500

    added redteam and blueteam logic and html add ons

--- GUI.js
+++ GUI.js
@@ -66,10 +66,30 @@ const arrOfPeople = [
     }
   }
   class blueTeammate {
-    constructor(){}
+    constructor(id, name, canThrowBall, canDodgeBall, hasPaid, isHealthy, yearsExperience, mascot, teamColor){
+      this.name = name;
+      this.id = id;
+      this.canThrowBall = canThrowBall;
+      this.canDodgeBall = canDodgeBall;
+      this.hasPaid = hasPaid;
+      this.isHealthy = isHealthy;
+      this.yearsExperience = yearsExperience;
+      this.mascot = 'Chicken';
+      this.teamColor = 'Blue'
+    }
   }
   class redTeammate {
-    constructor(){}
+    constructor(id, name, canThrowBall, canDodgeBall, hasPaid, isHealthy, yearsExperience, mascot, teamColor){
+      this.name = name;
+      this.id = id;
+      this.canThrowBall = canThrowBall;
+      this.canDodgeBall = canDodgeBall;
+      this.hasPaid = hasPaid;
+      this.isHealthy = isHealthy;
+      this.yearsExperience = yearsExperience;
+      this.mascot = 'Panther';
+      this.teamColor = 'Red'
+    }
   }
   
   const listPeopleChoices = () => {
@@ -92,7 +112,7 @@ const arrOfPeople = [
       let index = arrOfPeople.indexOf(person)
       arrOfPeople.splice(index, 1)
       //Generate Random Skill lvls for Players and push to list of players
-      const newPlayer = new player (person.id, person.name, Math.floor(Math.random()* 10 ), Math.floor(Math.random()* 10 ), Math.floor(Math.random()* 10 ), Math.floor(Math.random()* 10 ), Math.floor(Math.random()* 10 ))
+      const newPlayer = new player (person.id, person.name, Math.floor(Math.random()* 10 ), Math.floor(Math.random()* 10 ), 'yes', person.isHealthy, Math.floor(Math.random()* 10 ))
       listOfPlayers.push(newPlayer)
       // removes person from dom
       console.log(listOfPlayers)
@@ -100,20 +120,100 @@ const arrOfPeople = [
     while (listElement.hasChildNodes()) {  
       listElement.removeChild(listElement.firstChild);
     }
-    listPeopleChoices();
+    // listPeopleChoices();
     console.log(`li ${id} was clicked!`)
     const playerElement = document.getElementById('players')
-    listOfPlayers.map(person => {
+    // listOfPlayers.map(person => {
     const li = document.createElement("li")
     const button1 = document.createElement("button")
     button1.innerHTML = "Red Team"
-    button1.addEventListener('click', function() {makeRed(person.id)} )
+    button1.addEventListener('click', function() {makeRed(newPlayer.id)} )
     const button2 = document.createElement("button")
     button2.innerHTML = "Blue Team"
-    button2.addEventListener('click', function() {makeRed(person.id)} )
+    button2.addEventListener('click', function() {makeBlue(newPlayer.id)} )
     li.appendChild(button1)
     li.appendChild(button2)
-    li.appendChild(document.createTextNode(`Player #: ${person.id} ,  ${person.name},  ThrowBall Lvl:${person.canThrowBall} , CanDodge Lvl:${person.canDodgeBall}, IsHealthy: yes,  Years Playing:${person.yearsExperience}` ))
+    li.appendChild(document.createTextNode(`Player #: ${newPlayer.id} ,  ${newPlayer.name},  ThrowBall Lvl:${newPlayer.canThrowBall} , CanDodge Lvl:${newPlayer.canDodgeBall}, IsHealthy: yes,  Years Playing:${newPlayer.yearsExperience}` ))
     playerElement.append(li)
-    })
+    // })
     }
+
+    const makeRed = (id) => {
+      let person = listOfPlayers.find(function(player){
+          return player.id == id  
+      }) 
+      let index = listOfPlayers.indexOf(person)
+      listOfPlayers.splice(index, 1)
+    const newRed = new redTeammate (person.id, person.name, person.canThrowBall, person.canDodgeBall, person.hasPaid, person.isHealthy, person.yearsExperience, "Panther", "Red")
+    redTeam.push(newRed)
+    // removes person from dom
+    console.log(listOfPlayers)
+  const listElement = document.getElementById('players');
+  while (listElement.hasChildNodes()) {  
+    listElement.removeChild(listElement.firstChild);
+  }
+  const playerElement = document.getElementById('players')
+  listOfPlayers.map(person => {
+  const li = document.createElement("li")
+  const button1 = document.createElement("button")
+  button1.innerHTML = "Red Team"
+  button1.addEventListener('click', function() {makeRed(person.id)} )
+  const button2 = document.createElement("button")
+  button2.innerHTML = "Blue Team"
+  button2.addEventListener('click', function() {makeBlue(person.id)} )
+  li.appendChild(button1)
+  li.appendChild(button2)
+  li.appendChild(document.createTextNode(`Player #: ${person.id} ,  ${person.name},  ThrowBall Lvl:${person.canThrowBall} , CanDodge Lvl:${person.canDodgeBall}, IsHealthy: yes,  Years Playing:${person.yearsExperience}` ))
+  playerElement.append(li)
+  })
+  const redPlayerElement = document.getElementById('red')
+  // redTeam.map(person => {
+  const listItem = document.createElement("li")
+  const button = document.createElement("button")
+  button.innerHTML = "Remove Player"
+  button.addEventListener('click', function() {removePlayer(person.id)})
+  listItem.appendChild(button)
+  listItem.appendChild(document.createTextNode(`Player #: ${newRed.id} ,  ${newRed.name},  ThrowBall Lvl:${newRed.canThrowBall} , CanDodge Lvl:${newRed.canDodgeBall}, IsHealthy: yes,  Years Playing:${newRed.yearsExperience}, Mascot:${newRed.mascot}, Team Color:${newRed.teamColor}` ))
+  redPlayerElement.append(listItem)
+  // })
+  }
+
+  const makeBlue = (id) => {
+    let person = listOfPlayers.find(function(player){
+        return player.id == id  
+    }) 
+    let index = listOfPlayers.indexOf(person)
+    listOfPlayers.splice(index, 1)
+  const newBlue = new blueTeammate (person.id, person.name, person.canThrowBall, person.canDodgeBall, person.hasPaid, person.isHealthy, person.yearsExperience, 'Chicken', "Blue")
+  blueTeam.push(newBlue)
+  // removes person from dom
+  console.log(listOfPlayers)
+const listElement = document.getElementById('players');
+while (listElement.hasChildNodes()) {  
+  listElement.removeChild(listElement.firstChild);
+}
+const playerElement = document.getElementById('players')
+listOfPlayers.map(person => {
+const li = document.createElement("li")
+const button1 = document.createElement("button")
+button1.innerHTML = "Red Team"
+button1.addEventListener('click', function() {makeRed(person.id)} )
+const button2 = document.createElement("button")
+button2.innerHTML = "Blue Team"
+button2.addEventListener('click', function() {makeBlue(person.id)} )
+li.appendChild(button1)
+li.appendChild(button2)
+li.appendChild(document.createTextNode(`Player #: ${person.id} ,  ${person.name},  ThrowBall Lvl:${person.canThrowBall} , CanDodge Lvl:${person.canDodgeBall}, IsHealthy: yes,  Years Playing:${person.yearsExperience}` ))
+playerElement.append(li)
+})
+const bluePlayerElement = document.getElementById('blue')
+// redTeam.map(person => {
+const listItem = document.createElement("li")
+const button = document.createElement("button")
+button.innerHTML = "Remove Player"
+button.addEventListener('click', function() {removePlayer(person.id)})
+listItem.appendChild(button)
+listItem.appendChild(document.createTextNode(`Player #: ${newBlue.id} ,  ${newBlue.name},  ThrowBall Lvl:${newBlue.canThrowBall} , CanDodge Lvl:${newBlue.canDodgeBall}, IsHealthy: yes,  Years Playing:${newBlue.yearsExperience}, Mascot:${newBlue.mascot}, Team Color:${newBlue.teamColor}` ))
+bluePlayerElement.append(listItem)
+// })
+}
